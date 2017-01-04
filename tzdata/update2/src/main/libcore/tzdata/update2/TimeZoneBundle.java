b16ffb664387189d3f1c2ff97a457c3b2fcfeee2
Neil Fuller <nfuller@google.com>
2017-01-04 19:47:57 +0000

    Additional methods for TimeZoneBundleInstaller
    Adding methods, with tests, for TimeZoneBundleInstaller
    that allow interrogation of the current device state.
    
    Bug: 31008728
    Test: CtsLibcoreTestCases
    Change-Id: I8ff3824a1229df7a244650c01b8bdb42093951e1

--- tzdata/update2/src/main/libcore/tzdata/update2/TimeZoneBundle.java
+++ tzdata/update2/src/main/libcore/tzdata/update2/TimeZoneBundle.java
@@ -16,6 +16,7 @@
 package libcore.tzdata.update2;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -44,6 +45,12 @@ public final class TimeZoneBundle {
 
     private static final int BUFFER_SIZE = 8192;
 
+    /**
+     * Maximum size of entry getEntryContents() will pull into a byte array. To avoid exhausting
+     * heap memory when encountering unexpectedly large entries. 128k should be enough for anyone.
+     */
+    private static final long MAX_GET_ENTRY_CONTENTS_SIZE = 128 * 1024;
+
     private final byte[] bytes;
 
     public TimeZoneBundle(byte[] bytes) {
@@ -52,6 +59,42 @@ public final class TimeZoneBundle {
 
     public byte[] getBytes() {
         return bytes;
+    }
+
+    public BundleVersion getBundleVersion() throws BundleException, IOException {
+        byte[] contents = getEntryContents(
+                new ByteArrayInputStream(bytes), BUNDLE_VERSION_FILE_NAME);
+        if (contents == null) {
+            throw new BundleException("Bundle version file entry not found");
+        }
+        return BundleVersion.extractFromBytes(contents);
+    }
+
+    private static byte[] getEntryContents(InputStream is, String entryName) throws IOException {
+        try (ZipInputStream zipInputStream = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zipInputStream.getNextEntry()) != null) {
+                String name = entry.getName();
+
+                if (!entryName.equals(name)) {
+                    continue;
+                }
+                // Guard against massive entries consuming too much heap memory.
+                if (entry.getSize() > MAX_GET_ENTRY_CONTENTS_SIZE) {
+                    throw new IOException("Entry " + entryName + " too large: " + entry.getSize());
+                }
+                byte[] buffer = new byte[BUFFER_SIZE];
+                try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                    int count;
+                    while ((count = zipInputStream.read(buffer)) != -1) {
+                        baos.write(buffer, 0, count);
+                    }
+                    return baos.toByteArray();
+                }
+            }
+            // Entry not found.
+            return null;
+        }
     }
 
     public void extractTo(File targetDir) throws IOException {
@@ -119,5 +162,4 @@ public final class TimeZoneBundle {
 
         return true;
     }
-
 }
