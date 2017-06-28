0c192e4211dd5a10b7a59e1b427422ae13a7186c
Tobias Thierer <tobiast@google.com>
2017-06-28 15:45:26 +0100

    Fix NPE in URLClassLoader.getPermissions() introduced in Android N.
    From Android N until before this CL, URLClassLoader.getPermissions()
    could throw NPE because it unconditionally called endsWith() on the
    return value of FilePermission.getName(), which is stubbed to return
    null on Android. This CL fixes this by un-stubbing getName() to
    instead return the value that was passed to the constructor.
    Before Android N, URLClassLoader.getPermissions() used URL.getFile()
    rather than FilePermission.getName() to get the file path.
    
    Bug: 37380202
    Test: Checked that
          OldURLClassLoaderTest#test_getPermissions_fileURLConnection_doesNotThrow
          passes after this CL, but encounters NullPointerException during
          the getPermissions() call, when run on Android versions built
          before this CL (checked both immediately before this CL and on
          Android 7.0 Nougat).
    
    (cherry picked from commit bce13c50c300d35ebf91e6592951f7756c784736)
    
    Change-Id: Ic58a16736f851acad43e5e7d6aa023083f66de16

--- ojluni/src/main/java/java/io/FilePermission.java
+++ ojluni/src/main/java/java/io/FilePermission.java
@@ -35,7 +35,7 @@ import java.security.*;
 
 public final class FilePermission extends Permission implements Serializable {
 
-    public FilePermission(String path, String actions) { super(""); }
+    public FilePermission(String path, String actions) { super(path); }
 
     public boolean implies(Permission p) { return true; }
     public String getActions() { return null; }
