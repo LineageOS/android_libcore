491d88834d8af35b9701f92d972212d873dbb6a0
Adam Vartanian <flooey@google.com>
2017-10-10 11:43:32 +0100

    Deprecate BC implementations of duplicated algorithms
    The first step in deprecating the BC implementations of algorithms that
    are also provided by Conscrypt.  This checks whether the app's target
    API level is greater than a given threshold (currently O, but subject
    to change later) and disallows access to those algorithms when true.
    This limit only applies to requests from the system-created BC provider.
    Apps can install their own provider, in which case they get the
    implementations they requested.
    
    This doesn't yet implement the deprecation for Cipher instances, since
    the transformation scheme makes that more complicated.
    
    The ultimate removal of these algorithms, once a sufficient deprecation
    period has elapsed, is tracked in b/67761667.
    
    Bug: 67626877
    Test: cts -m CtsLibcoreTestCases
    Test: cts -m CtsLibcoreOkHttpTestCases
    Test: cts -m CtsLibcoreWycheproofBCTestCases
    Test: cts -m CtsLibcoreWycheproofConscryptTestCases
    Change-Id: I48f07226e66873a65859121af32028c1547952ac

--- luni/src/test/java/libcore/java/security/ProviderTest.java
+++ luni/src/test/java/libcore/java/security/ProviderTest.java
@@ -58,7 +58,27 @@ import javax.crypto.NoSuchPaddingException;
 import junit.framework.TestCase;
 import libcore.javax.crypto.MockKey;
 
+import dalvik.system.VMRuntime;
+import sun.security.jca.Providers;
+
 public class ProviderTest extends TestCase {
+
+    // Allow access to deprecated BC algorithms in this test, so we can ensure they
+    // continue to work
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        Providers.setMaximumAllowableApiLevelForBcDeprecation(
+                VMRuntime.getRuntime().getTargetSdkVersion());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        Providers.setMaximumAllowableApiLevelForBcDeprecation(
+                Providers.DEFAULT_MAXIMUM_ALLOWABLE_TARGET_API_LEVEL_FOR_BC_DEPRECATION);
+        super.tearDown();
+    }
+
     private static final boolean LOG_DEBUG = false;
 
     /**
