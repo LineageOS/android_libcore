efbd4adaf39495f02de043866e3980d0e8cf08d2
Enrico Granata <egranata@google.com>
2017-11-08 11:07:06 -0800

    Fix OsTest for different possible setgroups() return values.
    setgroups() can return EINVAL if the number of
    groups passed is > NGROUPS_MAX.
    
    This means that the call in this test could fail
    with EINVAL is NGROUPS_MAX is == 0.
    
    Address this possibility by checking and choosing
    an appropriate error code to test against.
    
    Bug: 69023954
    Test: vogar OsTest
    Change-Id: Id03cdb73a9cc3df31f22b25dc28079aac609a35f

--- luni/src/test/java/libcore/libcore/io/OsTest.java
+++ luni/src/test/java/libcore/libcore/io/OsTest.java
@@ -947,11 +947,14 @@ public class OsTest extends TestCase {
   }
 
   public void test_setgroups() throws Exception {
+    final long ngroupsMax = Libcore.os.sysconf(_SC_NGROUPS_MAX);
+    final int expectedError = ngroupsMax == 0 ? EINVAL : EPERM;
+
     try {
       Libcore.os.setgroups(new int[] {-1});
       fail();
     } catch (ErrnoException expected) {
-      assertEquals(EPERM, expected.errno);
+      assertEquals(expectedError, expected.errno);
     }
   }
 
