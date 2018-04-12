a9d6051d032667ff6dff4353334166dd7250c007
Victor Chang <vichang@google.com>
2018-04-12 19:07:12 +0100

    Add min API level of 26 for the named capaturing in javadoc
    - Named capaturing is only supported since API level 26 (Oreo)
    - Although the constructs are available since ICU 55 (Marshmallow),
      the API in Matcher wasn't added until API level 26, and there was
      no CTS before Oreo. Thus, Android only officially supports it since O.
    
    Bug: 71727710
    Test: m docs
    Merged-In: Ib04f3fcfb831be50f4e77b4ca8c2952349e1db77
    Change-Id: Ib04f3fcfb831be50f4e77b4ca8c2952349e1db77
    (cherry picked from commit d88def68d81a371284ed1c25a7b038cfefe0ccf3)

--- ojluni/src/main/java/java/util/regex/Pattern.java
+++ ojluni/src/main/java/java/util/regex/Pattern.java
@@ -40,6 +40,7 @@ import java.util.stream.StreamSupport;
 
 import libcore.util.EmptyArray;
 
+// Android-changed: Add min API level of 26 for the named capaturing in javadoc
 /**
  * A compiled representation of a regular expression.
  *
@@ -314,7 +315,7 @@ import libcore.util.EmptyArray;
  *
  * <tr><td valign="bottom" headers="construct backref"><tt>\</tt><i>k</i>&lt;<i>name</i>&gt;</td>
  *     <td valign="bottom" headers="matches">Whatever the
- *     <a href="#groupname">named-capturing group</a> "name" matched</td></tr>
+ *     <a href="#groupname">named-capturing group</a> "name" matched. Only available for API 26 or above</td></tr>
  *
  * <tr><th>&nbsp;</th></tr>
  * <tr align="left"><th colspan="2" id="quot">Quotation</th></tr>
@@ -331,7 +332,7 @@ import libcore.util.EmptyArray;
  * <tr align="left"><th colspan="2" id="special">Special constructs (named-capturing and non-capturing)</th></tr>
  *
  * <tr><td valign="top" headers="construct special"><tt>(?&lt;<a href="#groupname">name</a>&gt;</tt><i>X</i><tt>)</tt></td>
- *     <td headers="matches"><i>X</i>, as a named-capturing group</td></tr>
+ *     <td headers="matches"><i>X</i>, as a named-capturing group. Only available for API 26 or above.</td></tr>
  * <tr><td valign="top" headers="construct special"><tt>(?:</tt><i>X</i><tt>)</tt></td>
  *     <td headers="matches"><i>X</i>, as a non-capturing group</td></tr>
  * <tr><td valign="top" headers="construct special"><tt>(?idmsuxU-idmsuxU)&nbsp;</tt></td>
@@ -493,7 +494,8 @@ import libcore.util.EmptyArray;
  *
  * <a name="groupname">
  * <h5> Group name </h5>
- * <p>A capturing group can also be assigned a "name", a <tt>named-capturing group</tt>,
+ * <p>The constructs and APIs are available since API level 26. A capturing group
+ * can also be assigned a "name", a <tt>named-capturing group</tt>,
  * and then be back-referenced later by the "name". Group names are composed of
  * the following characters. The first character must be a <tt>letter</tt>.
  *
