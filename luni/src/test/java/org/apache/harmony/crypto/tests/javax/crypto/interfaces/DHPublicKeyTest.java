d061b8f01396fba134eb928bbf2f71fd7db9f213
Adam Vartanian <flooey@google.com>
2019-02-20 16:17:18 +0000

    Delete several DH key tests
    The Bouncy Castle upgrade added a subclass of DHParameterSpec that
    includes more information about the key parameters.  There's no reason
    to bar extending those classes, so we no longer need the assertion
    that it's precisely the base class.  Once that's gone, the rest of the
    assertions are largely just testing the signatures of the methods, so
    we can just delete the test cases entirely.
    
    Test: cts -m CtsLibcoreTestCases -t org.apache.harmony.crypto.tests.javax.crypto.interfaces
    Change-Id: I4f69a7527d7f5eace1c3bbaf662cb9d584c28f5b

--- luni/src/test/java/org/apache/harmony/crypto/tests/javax/crypto/interfaces/DHPublicKeyTest.java
+++ luni/src/test/java/org/apache/harmony/crypto/tests/javax/crypto/interfaces/DHPublicKeyTest.java
@@ -50,45 +50,6 @@ public class DHPublicKeyTest extends TestCase {
                 -6628103563352519193L);
     }
 
-    public void test_getParams_initToHardCoded() throws Exception {
-        // (p, g) values from RFC 7919, Appendix A (2048-bit group)
-        String pStr = "FFFFFFFFFFFFFFFFADF85458A2BB4A9AAFDC5620273D3CF1" +
-                "D8B9C583CE2D3695A9E13641146433FBCC939DCE249B3EF9" +
-                "7D2FE363630C75D8F681B202AEC4617AD3DF1ED5D5FD6561" +
-                "2433F51F5F066ED0856365553DED1AF3B557135E7F57C935" +
-                "984F0C70E0E68B77E2A689DAF3EFE8721DF158A136ADE735" +
-                "30ACCA4F483A797ABC0AB182B324FB61D108A94BB2C8E3FB" +
-                "B96ADAB760D7F4681D4F42A3DE394DF4AE56EDE76372BB19" +
-                "0B07A7C8EE0A6D709E02FCE1CDF7E2ECC03404CD28342F61" +
-                "9172FE9CE98583FF8E4F1232EEF28183C3FE3B1B4C6FAD73" +
-                "3BB5FCBC2EC22005C58EF1837D1683B2C6F34A26C1B2EFFA" +
-                "886B423861285C97FFFFFFFFFFFFFFFF";
-        BigInteger p = new BigInteger(new BigInteger(pStr, 16).toByteArray());
-        BigInteger g = BigInteger.valueOf(2);
-        KeyPairGenerator kg = KeyPairGenerator.getInstance("DH");
-        kg.initialize(new DHParameterSpec(p, g), new SecureRandom());
-        check_getParams(kg);
-    }
-
-    public void test_getParams_initToRandom192bit() throws Exception {
-        KeyPairGenerator kg = KeyPairGenerator.getInstance("DH");
-        // DH group generation is slow, so we test with a small (insecure) value
-        kg.initialize(192);
-        check_getParams(kg);
-    }
-
-    private void check_getParams(KeyPairGenerator kg) {
-        KeyPair kp1 = kg.genKeyPair();
-        KeyPair kp2 = kg.genKeyPair();
-        DHPublicKey pk1 = (DHPublicKey) kp1.getPublic();
-        DHPublicKey pk2 = (DHPublicKey) kp2.getPublic();
-
-        assertTrue(pk1.getY().getClass().getCanonicalName().equals("java.math.BigInteger"));
-        assertTrue(pk2.getParams().getClass().getCanonicalName().equals("javax.crypto.spec.DHParameterSpec"));
-        assertFalse(pk1.equals(pk2));
-        assertTrue(pk1.getY().equals(pk1.getY()));
-    }
-
     public class checkDHPublicKey implements DHPublicKey {
         public String getAlgorithm() {
             return "SecretKey";
