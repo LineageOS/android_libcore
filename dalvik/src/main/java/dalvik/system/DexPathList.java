8f712189dfc02285573337e2b4ab17678011db14
Jiyong Park <jiyong@google.com>
2019-08-28 23:06:42 +0900

    Don't give up when loader.findLibrary() fails
    When System.loadLibrary(libname) is called, the library file for the
    libname is searched in the native library paths set to the current
    classloader. Previously, if the search was failed, actual loading of the
    lib was not attempted at all. This however is too restrictive in Android
    where the dynamic linker that actually is responsible for finding and
    loading libraries has a concept of linker namespaces. With linker
    namespaces, whether a library can be loaded or not is not simply
    determined by checking whether the lib is found in one of the library
    search paths. A lib can be loaded also when there is a link from the
    current linker namespace to another linker namespace with the library's
    soname specified over the link. In that case, the library is searched in
    the search paths of the target linker namespace.
    
    So, a failure of loader.findLibrary() doesn't necessary mean that the
    library is never loadable. This caused a problem like b/93333337, which
    required us to do the non-trivial ad-hoc change:
    cfe38cdb1cc45c8b7dcbe4f39329551f6602b9ce in frameworks/base.
    
    In this change, even when loader.findLibrary() fails, we try to load the
    library with its soname, not with the absolute file path.
    
    Bug: 109720125
    Test: System.loadLibrary("adsprpc") is successful in Pixel (because
    libadsprpc.so is in Pixel's vendor public lib list)
    Test: atest cts/tests/tests/jni
    
    Change-Id: Ia2525e736f91460a50f2def3d6a04bf3cf8c221d

--- dalvik/src/main/java/dalvik/system/DexPathList.java
+++ dalvik/src/main/java/dalvik/system/DexPathList.java
@@ -603,8 +603,12 @@ public final class DexPathList {
                 return path;
             }
         }
-
-        return null;
+        // Don't give up even if we failed to find the library in the native lib paths.
+        // The underlying dynamic linker might be able to find the lib in one of the linker
+        // namespaces associated with the current linker namespace. In order to give the
+        // dynamic linker a chance, proceed to load the library with its soname, which
+        // is the fileName.
+        return fileName;
     }
 
     /**
