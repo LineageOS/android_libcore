d74c1cd61ff9c240423a1f788c82dc844aad2d74
Usama Arif <usama.arif@linaro.org>
2019-10-30 14:58:40 +0000

    Mask result with qNaN if input to rint is NaN
    This CL masks the output with qNaN for rint of FP16 NaN values
    i.e. mask the most significant mantissa bit with 1. In ARM hardware,
    the rest of the mantissa bits are still processed when input is NaN,
    therefore the mask comes at the end of the rint function.
    
    Test: cts-tradefed  run singleCommand cts-dev --module CtsLibcoreTestCases --test libcore.libcore.util.FP16Test
    Change-Id: I431ea90e22b813491d2cdb95daf9b186c7ca15f2

--- luni/src/main/java/libcore/util/FP16.java
+++ luni/src/main/java/libcore/util/FP16.java
@@ -249,6 +249,12 @@ public class FP16 {
             result += (1 << (abs - 1));
             result &= ~mask;
         }
+        if (isNaN((short) result)) {
+            // if result is NaN mask with qNaN
+            // (i.e. mask the most significant mantissa bit with 1)
+            // to comply with hardware implementations (ARM64, Intel, etc).
+            result |= NaN;
+        }
 
         return (short) result;
     }
