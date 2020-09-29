ae4ef95f799e36288067f73b599504f0aac90662
Nicolas Geoffray <ngeoffray@google.com>
2020-09-29 16:17:20 +0100

    Document CorePlatformAPI method.
    Test: n/a
    Bug: 165361264
    Change-Id: Ib749c806b1f4d4cd3edb387f2d67d89ccc3b9d2d

--- dalvik/src/main/java/dalvik/system/PathClassLoader.java
+++ dalvik/src/main/java/dalvik/system/PathClassLoader.java
@@ -65,6 +65,32 @@ public class PathClassLoader extends BaseDexClassLoader {
     }
 
     /**
+     * Creates a {@code PathClassLoader} that operates on two given
+     * lists of files and directories. The entries of the first list
+     * should be one of the following:
+     *
+     * <ul>
+     * <li>JAR/ZIP/APK files, possibly containing a "classes.dex" file as
+     * well as arbitrary resources.
+     * <li>Raw ".dex" files (not inside a zip file).
+     * </ul>
+     *
+     * The entries of the second list should be directories containing
+     * native library files.
+     *
+     * @param dexPath the list of jar/apk files containing classes and
+     * resources, delimited by {@code File.pathSeparator}, which
+     * defaults to {@code ":"} on Android
+     * @param librarySearchPath the list of directories containing native
+     * libraries, delimited by {@code File.pathSeparator}; may be
+     * {@code null}
+     * @param parent the parent class loader
+     * @param sharedLibraryLoaders class loaders of Java shared libraries
+     * used by this new class loader. The shared library loaders are always
+     * checked before the {@code dexPath} when looking
+     * up classes and resources.
+     */
+    /**
      * @hide
      */
     @libcore.api.CorePlatformApi
