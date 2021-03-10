14babcff0ca1bdde8f68074493279484902f52bc
Victor Chang <vichang@google.com>
2021-03-10 17:01:50 +0000

    Add API coverage for java.security methods
    Bug: 182166453
    Test: atest CtsLibcoreTestCases:libcore.java.security
    Change-Id: Ic528c887c6ef85dcd3b814b38a8cf756a924675a

--- luni/src/test/java/libcore/java/security/ProviderTest.java
+++ luni/src/test/java/libcore/java/security/ProviderTest.java
@@ -36,6 +36,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -1068,6 +1069,21 @@ public class ProviderTest extends TestCaseWithRules {
         p.put("class1.algorithm1", "impl1");
         assertEquals("impl1", p.getOrDefault("class1.algorithm1", "default"));
         assertEquals("default", p.getOrDefault("thisIsNotInTheProvider", "default"));
+    }
+
+    public void test_elements() {
+        Provider p = new MockProvider("MockProvider");
+        p.put("class1.algorithm1", "impl1");
+        Enumeration<Object> elements = p.elements();
+        boolean isImpl1Found = false;
+        while (elements.hasMoreElements()) {
+            if ("impl1".equals(elements.nextElement())) {
+                isImpl1Found = true;
+                break;
+            }
+        }
+
+        assertTrue("impl1 is not found.", isImpl1Found);
     }
 
     private static class Pair<A, B> {
