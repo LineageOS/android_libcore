49cdb17ba9aaad16ff1d42945d32667e7857caff
Hans Boehm <hboehm@google.com>
2021-04-29 16:16:39 +0000

    Revert^2 "Remove VMRuntime.concurrentGC"
    548136c7a3c98edaf0f325671a23dc7150315d13
    
    Change-Id: Ie325a2e258e744dc0411b225e78fa6c7ed2aed08

--- libart/src/main/java/dalvik/system/VMRuntime.java
+++ libart/src/main/java/dalvik/system/VMRuntime.java
@@ -631,7 +631,6 @@ public final class VMRuntime {
     @libcore.api.CorePlatformApi
     public native void requestConcurrentGC();
 
-    public native void concurrentGC();
     public native void requestHeapTrim();
     public native void trimHeap();
     public native void startHeapTaskProcessor();
