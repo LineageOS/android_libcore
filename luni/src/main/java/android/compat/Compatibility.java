50697278fb7ee12c7620ad6238a2002b6995cdc8
Nikita Iashchenko <nikitai@google.com>
2021-04-29 17:12:59 +0100

    Rename getters and setters android.compat.Compatibility.ChangeConfig
    As a part of api review rename getters and setters to comply guidelines.
    
    Bug: 183512903
    Test: m droid
    Change-Id: I7180b6abfbe2db69101da1746ba1424d195489dc

--- luni/src/main/java/android/compat/Compatibility.java
+++ luni/src/main/java/android/compat/Compatibility.java
@@ -25,6 +25,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import libcore.util.NonNull;
 
 /**
  * Internal APIs for logging and gating compatibility changes.
@@ -138,7 +139,7 @@ public final class Compatibility {
         private final Set<Long> enabled;
         private final Set<Long> disabled;
 
-        public ChangeConfig(Set<Long> enabled, Set<Long> disabled) {
+        public ChangeConfig(@NonNull Set<@NonNull Long> enabled, @NonNull Set<@NonNull Long> disabled) {
             this.enabled = Objects.requireNonNull(enabled);
             this.disabled = Objects.requireNonNull(disabled);
             if (enabled.contains(null)) {
@@ -168,19 +169,19 @@ public final class Compatibility {
             return result;
         }
 
-        public long[] forceEnabledChangesArray() {
+        public @NonNull long[] getEnabledChangesArray() {
             return toLongArray(enabled);
         }
 
-        public long[] forceDisabledChangesArray() {
+        public @NonNull long[] getDisabledChangesArray() {
             return toLongArray(disabled);
         }
 
-        public Set<Long> forceEnabledSet() {
+        public @NonNull Set<@NonNull Long> getEnabledSet() {
             return Collections.unmodifiableSet(enabled);
         }
 
-        public Set<Long> forceDisabledSet() {
+        public @NonNull Set<@NonNull Long> getDisabledSet() {
             return Collections.unmodifiableSet(disabled);
         }
 
