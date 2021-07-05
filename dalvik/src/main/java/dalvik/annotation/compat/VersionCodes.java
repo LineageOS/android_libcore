32746bb55870f48e1a7aaeb00a4cb89ebea19429
Orion Hodson <oth@google.com>
2021-07-05 12:00:24 +0100

    Update version code definitions
    (cherry picked from change 7efd2b930167d6d13bd2a2f357f53cc04884f49f)
    
    Bug: 171506470
    Test: TH
    Merged-In: Ic02149e33ced7230dbe0804ea07dbdc30bcde0c5
    Change-Id: I442846cf2eebe789ef9f981330a6c99d7aa081b6

--- dalvik/src/main/java/dalvik/annotation/compat/VersionCodes.java
+++ dalvik/src/main/java/dalvik/annotation/compat/VersionCodes.java
@@ -65,4 +65,10 @@ public class VersionCodes {
      */
     @IntraCoreApi
     public static final int R = 30;
+
+    /**
+     * The version code for Android S (API version 31).
+     */
+    @IntraCoreApi
+    public static final int S = 31;
 }
