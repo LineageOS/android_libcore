5b7f7c74aa6c94c3282de65821a41cbcd26dc22d
Nikita Iashchenko <nikitai@google.com>
2021-10-04 18:18:01 +0100

    Add API coverage for javax.security.auth
    API list:
     * javax.security.auth.AuthPermission.AuthPermission(java.lang.String)
     * javax.security.auth.AuthPermission.AuthPermission(java.lang.String, java.lang.String)
     * javax.security.auth.PrivateCredentialPermission.PrivateCredentialPermission(java.lang.String, java.lang.String)
     * javax.security.auth.PrivateCredentialPermission.getActions()
     * javax.security.auth.PrivateCredentialPermission.getCredentialClass()
     * javax.security.auth.PrivateCredentialPermission.getPrincipals()
     * javax.security.auth.PrivateCredentialPermission.implies(java.security.Permission)
     * javax.security.auth.Subject.getPrincipals(java.lang.Class)
     * javax.security.auth.Subject.getPrivateCredentials(java.lang.Class)
     * javax.security.auth.Subject.getPublicCredentials(java.lang.Class)
     * javax.security.auth.Subject.isReadOnly()
     * javax.security.auth.Subject.setReadOnly()
     * javax.security.auth.SubjectDomainCombiner.combine(java.security.ProtectionDomain[], java.security.ProtectionDomain[])
     * javax.security.auth.SubjectDomainCombiner.getSubject()
    
    Bug: 182164686
    Test: atest CtsLibcoreTestCases:libcore.javax.security.auth.{\
                AuthPermissionTest,PrivateCredentialPermissionTest,\
                SubjectDomainCombinerTest,SubjectTest}
    Merged-In: Iba7dd6625ab957b8f408b9ff0ba3de3b967f384c
    Change-Id: Iba7dd6625ab957b8f408b9ff0ba3de3b967f384c

---
+++ luni/src/test/java/libcore/javax/security/auth/SubjectTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package libcore.javax.security.auth;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.security.Principal;
+import java.util.Set;
+import javax.security.auth.Subject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Android does not support {@link java.lang.SecurityManager} and has its implementations stubbed.
+ * See comments in {@link java.lang.SecurityManager} for details.
+ *
+ * This test is added primarily for code coverage.
+ */
+@RunWith(JUnit4.class)
+public class SubjectTest {
+
+    private Set<Principal> set;
+    private Subject subject;
+
+    @Before
+    public void setUp() {
+        set = Set.of(new PrincipalImpl());
+        subject = new Subject(true, set, set, set);
+    }
+
+    @Test
+    public void getPrincipals() {
+        assertEquals(set, subject.getPrincipals());
+    }
+
+    @Test
+    public void getPrivateCredentials() {
+        assertEquals(set, subject.getPrivateCredentials());
+    }
+
+    @Test
+    public void getPublicCredentials() {
+        assertEquals(set, subject.getPublicCredentials());
+    }
+
+    @Test
+    public void isReadOnly() {
+        assertTrue(subject.isReadOnly());
+
+        subject = new Subject(false, set, set, set);
+        assertFalse(subject.isReadOnly());
+    }
+
+    @Test
+    public void setReadOnly() {
+        subject = new Subject(false, set, set, set);
+        assertFalse(subject.isReadOnly());
+
+        subject.setReadOnly();
+        assertTrue(subject.isReadOnly());
+    }
+
+    private static final class PrincipalImpl implements Principal {
+
+        @Override
+        public String getName() {
+            return "";
+        }
+    }
+}
