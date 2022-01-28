f5183feab474b7445e8af519b1472f7cc178e8ff
Orion Hodson <oth@google.com>
2022-01-28 18:15:48 +0000

    EmulatedStackFrame: drop unnecessary callsiteType field
    This change drops the EmulatedStackFrame.callsiteType field and
    removes the concept of a callsite transform.
    
    Bug: N/A
    Test: art/test.py --host
    Test: atest CtsLibcoreOjTestCases:test.java.lang.invoke
    Test: atest CtsLibcoreTestCases:libcore.java.lang.invoke
    Change-Id: I7e294d5231126e276ad5d57428c00e5eb4f5f525

--- ojluni/src/main/java/java/lang/invoke/Transformers.java
+++ ojluni/src/main/java/java/lang/invoke/Transformers.java
@@ -639,7 +639,7 @@ public class Transformers {
         private final Class<?> arrayType;
 
         /*package*/ VarargsCollector(MethodHandle target) {
-            super(target.type(), MethodHandle.INVOKE_CALLSITE_TRANSFORM);
+            super(target.type());
 
             Class<?>[] parameterTypes = target.type().ptypes();
             if (!lastParameterTypeIsAnArray(parameterTypes)) {
@@ -1181,17 +1181,6 @@ public class Transformers {
 
         @Override
         public void transform(EmulatedStackFrame emulatedStackFrame) throws Throwable {
-            // We need to artificially throw a WrongMethodTypeException here because we
-            // can't call invokeExact on the target inside the transformer.
-            if (isExactInvoker) {
-                MethodType callsiteType =
-                        emulatedStackFrame.getCallsiteType().dropParameterTypes(0, 1);
-                if (!exactMatch(callsiteType, targetType)) {
-                    throw new WrongMethodTypeException(
-                            "Wrong type, Expected: " + targetType + " was: " + callsiteType);
-                }
-            }
-
             // The first argument to the stack frame is the handle that needs to be invoked.
             MethodHandle target = emulatedStackFrame.getReference(0, MethodHandle.class);
 
@@ -1200,7 +1189,11 @@ public class Transformers {
             emulatedStackFrame.copyRangeTo(targetFrame, copyRange, 0, 0);
 
             // Finally, invoke the handle and copy the return value.
-            invokeFromTransform(target, targetFrame);
+            if (isExactInvoker) {
+                invokeExactFromTransform(target, targetFrame);
+            } else {
+                invokeFromTransform(target, targetFrame);
+            }
             targetFrame.copyReturnValueTo(emulatedStackFrame);
         }
 
