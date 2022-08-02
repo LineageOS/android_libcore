61fbe2557a2618b4e922807aadd78fee059bb6f6
Nikita Iashchenko <nikitai@google.com>
2022-08-02 17:55:30 +0100

    OpenJDK 17: sun.util.calendar.BaseCalendar.java
    This is part of merging upstream changes from OpenJDK 17.0.3-ga. This CL
    updates sun.util.calendar.BaseCalendar.java class.
    
    Bug: 241096650
    Test: m
    Change-Id: Ibb4e1133d738909a9c43aa3c4f854f6d8c5b03a0

--- ojluni/src/main/java/sun/util/calendar/BaseCalendar.java
+++ ojluni/src/main/java/sun/util/calendar/BaseCalendar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,7 +28,7 @@ package sun.util.calendar;
 import java.util.TimeZone;
 
 /**
- * The <code>BaseCalendar</code> provides basic calendar calculation
+ * The {@code BaseCalendar} provides basic calendar calculation
  * functions to support the Julian, Gregorian, and Gregorian-based
  * calendar systems.
  *
@@ -277,6 +277,10 @@ public abstract class BaseCalendar extends AbstractCalendar {
             long xm = 1L - month;
             year -= (int)((xm / 12) + 1);
             month = 13 - (xm % 12);
+            if (month == 13) {
+                year++;
+                month = 1;
+            }
             bdate.setNormalizedYear(year);
             bdate.setMonth((int) month);
         } else if (month > DECEMBER) {
@@ -290,11 +294,11 @@ public abstract class BaseCalendar extends AbstractCalendar {
     /**
      * Returns 366 if the specified date is in a leap year, or 365
      * otherwise This method does not perform the normalization with
-     * the specified <code>CalendarDate</code>. The
-     * <code>CalendarDate</code> must be normalized to get a correct
+     * the specified {@code CalendarDate}. The
+     * {@code CalendarDate} must be normalized to get a correct
      * value.
      *
-     * @param a <code>CalendarDate</code>
+     * @param date a {@code CalendarDate}
      * @return a year length in days
      * @throws ClassCastException if the specified date is not a
      * {@link BaseCalendar.Date}
@@ -412,7 +416,7 @@ public abstract class BaseCalendar extends AbstractCalendar {
 
     /**
      * Calculates calendar fields and store them in the specified
-     * <code>CalendarDate</code>.
+     * {@code CalendarDate}.
      */
     // should be 'protected'
     public void getCalendarDateFromFixedDate(CalendarDate date,
