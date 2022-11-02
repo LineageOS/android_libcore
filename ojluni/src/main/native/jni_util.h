0edf7c1587750a7feed59792664711daa51a82ca
Victor Chang <vichang@google.com>
2022-11-02 11:15:21 +0000

    Revert java.lang.Process* OpenJDK 11 update
    Revert "Temporarily hide new java.lang.Process* APIs"
    This reverts commit 17035ac8a22260ca2320511ef94da7a69bfd895f.
    
    Revert "Merge java.lang.Process* from jdk-11.0.13-ga into the aosp/master branch"
    This reverts commit 837b789147d8f67538be3cfac95aea1343eaf36d, reversing
    changes made to d6968df2c3bf983e771c4cde03e05956e35c1ae9.
    
    Bug: 256033367
    Bug: 255948653
    Test: m droid
    Change-Id: Ifb713c8fd28d07d5e30c8885d5f1f69dc1cf6e4a

--- ojluni/src/main/native/jni_util.h
+++ ojluni/src/main/native/jni_util.h
@@ -290,22 +290,6 @@ JNU_NotifyAll(JNIEnv *env, jobject object);
         }                                       \
     } while (0)                                 \
 
-#define CHECK_NULL_THROW_NPE(env, x, msg)         \
-    do {                                        \
-        if ((x) == NULL) {                      \
-           JNU_ThrowNullPointerException((env), (msg));\
-           return;                              \
-        }                                       \
-    } while(0)                                  \
-
-#define CHECK_NULL_THROW_NPE_RETURN(env, x, msg, z)\
-    do {                                        \
-        if ((x) == NULL) {                      \
-           JNU_ThrowNullPointerException((env), (msg));\
-           return (z);                          \
-        }                                       \
-    } while(0)                                  \
-
 #define CHECK_NULL_RETURN(x, y)                 \
     do {                                        \
         if ((x) == NULL) {                      \
@@ -313,35 +297,7 @@ JNU_NotifyAll(JNIEnv *env, jobject object);
         }                                       \
     } while (0)                                 \
 
-#ifdef __cplusplus
-#define JNU_CHECK_EXCEPTION(env)                \
-    do {                                        \
-        if ((env)->ExceptionCheck()) {          \
-            return;                             \
-        }                                       \
-    } while (0)                                 \
 
-#define JNU_CHECK_EXCEPTION_RETURN(env, y)      \
-    do {                                        \
-        if ((env)->ExceptionCheck()) {          \
-            return (y);                         \
-        }                                       \
-    } while (0)
-#else
-#define JNU_CHECK_EXCEPTION(env)                \
-    do {                                        \
-        if ((*env)->ExceptionCheck(env)) {      \
-            return;                             \
-        }                                       \
-    } while (0)                                 \
-
-#define JNU_CHECK_EXCEPTION_RETURN(env, y)      \
-    do {                                        \
-        if ((*env)->ExceptionCheck(env)) {      \
-            return (y);                         \
-        }                                       \
-    } while (0)
-#endif /* __cplusplus */
 /************************************************************************
  * Debugging utilities
  */
