7f45c0e2a89446cc3200a0dad6d483f711097d08
Sorin Basca <sorinbasca@google.com>
2022-11-22 11:34:51 +0000

    Add script to merge upstream package
    Test: ojluni_merge_package java.util.concurrent.atomic
    Test: ojluni_cleanup_merge_branches
    Change-Id: I2dfdd53b389113b71c8f8a240bbeb8e791e3ccfa

---
+++ tools/expected_upstream/ojluni_cleanup_merged_branches.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script that cleans up branches created by ojluni_merge_to_master. They are
+# usually name something like 'expected_upstream_e0bywxovb5'.
+
+if [[ -z "${ANDROID_BUILD_TOP}" ]]
+then
+  echo -e "ANDROID_BUILD_TOP not found. You need to run lunch first."
+  exit 1
+fi
+
+pushd "${ANDROID_BUILD_TOP}/libcore"
+
+for br in $(git branch | grep -E "^\s*expected_upstream_[a-z0-9]+$")
+do
+  echo git branch -D ${br}
+  git branch -D ${br}
+done
+
+popd
