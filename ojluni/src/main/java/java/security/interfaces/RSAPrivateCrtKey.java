2e7df20680fcf10bef03a8ae4ca8bc0f4be70841
Almaz Mingaleev <mingaleev@google.com>
2023-02-02 12:53:31 +0000

    Import java.security.interfaces from jdk-17.0.5-ga
    List of files:
      ojluni/src/main/java/java/security/interfaces/DSAKeyPairGenerator.java
      ojluni/src/main/java/java/security/interfaces/DSAPrivateKey.java
      ojluni/src/main/java/java/security/interfaces/DSAPublicKey.java
      ojluni/src/main/java/java/security/interfaces/ECPrivateKey.java
      ojluni/src/main/java/java/security/interfaces/ECPublicKey.java
      ojluni/src/main/java/java/security/interfaces/RSAKey.java
      ojluni/src/main/java/java/security/interfaces/RSAMultiPrimePrivateCrtKey.java
      ojluni/src/main/java/java/security/interfaces/RSAPrivateCrtKey.java
      ojluni/src/main/java/java/security/interfaces/RSAPrivateKey.java
      ojluni/src/main/java/java/security/interfaces/RSAPublicKey.java
      ojluni/src/main/java/java/security/interfaces/package-info.java
    
    Generated by tools/expected_upstream/ojluni_merge_to_master.py
    
    Bug: 260847206
    Test: N/A
    Change-Id: I817dc41e044c8c80bef73bc4b5e088838015be56

--- ojluni/src/main/java/java/security/interfaces/RSAPrivateCrtKey.java
+++ ojluni/src/main/java/java/security/interfaces/RSAPrivateCrtKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -45,7 +45,13 @@ public interface RSAPrivateCrtKey extends RSAPrivateKey {
      * The type fingerprint that is set to indicate
      * serialization compatibility with a previous
      * version of the type.
+     *
+     * @deprecated A {@code serialVersionUID} field in an interface is
+     * ineffectual. Do not use; no replacement.
      */
+    @Deprecated
+    @SuppressWarnings("serial")
+    @java.io.Serial
     static final long serialVersionUID = -5682214253527700368L;
 
     /**
@@ -57,7 +63,7 @@ public interface RSAPrivateCrtKey extends RSAPrivateKey {
 
     /**
      * Returns the primeP.
-
+     *
      * @return the primeP
      */
     public BigInteger getPrimeP();
