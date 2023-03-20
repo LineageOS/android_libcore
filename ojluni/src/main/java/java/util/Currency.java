e1668ca2d57d6e3e79b0365f9debb7c7622f4c5c
Almaz Mingaleev <mingaleev@google.com>
2023-03-20 14:31:58 +0000

    Import Currency from jdk-17.0.6-ga
    List of files:
      ojluni/src/main/java/java/util/Currency.java
    
    Generated by tools/expected_upstream/ojluni_merge_to_master.py
    
    Bug: 270674727
    Test: N/A
    Change-Id: Ic9615b6af41b7c524eb3adbcdd69a9d878e6c393

--- ojluni/src/main/java/java/util/Currency.java
+++ ojluni/src/main/java/java/util/Currency.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -55,12 +55,12 @@ import sun.util.logging.PlatformLogger;
  * ISO web site</a> for more information.
  * <p>
  * The class is designed so that there's never more than one
- * <code>Currency</code> instance for any given currency. Therefore, there's
- * no public constructor. You obtain a <code>Currency</code> instance using
- * the <code>getInstance</code> methods.
+ * {@code Currency} instance for any given currency. Therefore, there's
+ * no public constructor. You obtain a {@code Currency} instance using
+ * the {@code getInstance} methods.
  * <p>
  * Users can supersede the Java runtime currency data by means of the system
- * property {@code java.util.currency.data}. If this system property is
+ * property {@systemProperty java.util.currency.data}. If this system property is
  * defined then its value is the location of a properties file, the contents of
  * which are key/value pairs of the ISO 3166 country codes and the ISO 4217
  * currency data respectively.  The value part consists of three ISO 4217 values
@@ -111,8 +111,10 @@ import sun.util.logging.PlatformLogger;
  * @see java.math.BigDecimal
  * @since 1.4
  */
+@SuppressWarnings("removal")
 public final class Currency implements Serializable {
 
+    @java.io.Serial
     private static final long serialVersionUID = -158308464356906721L;
 
     /**
@@ -273,7 +275,7 @@ public final class Currency implements Serializable {
 
 
     /**
-     * Constructs a <code>Currency</code> instance. The constructor is private
+     * Constructs a {@code Currency} instance. The constructor is private
      * so that we can insure that there's never more than one instance for a
      * given currency.
      */
@@ -284,12 +286,12 @@ public final class Currency implements Serializable {
     }
 
     /**
-     * Returns the <code>Currency</code> instance for the given currency code.
+     * Returns the {@code Currency} instance for the given currency code.
      *
      * @param currencyCode the ISO 4217 code of the currency
-     * @return the <code>Currency</code> instance for the given currency code
-     * @exception NullPointerException if <code>currencyCode</code> is null
-     * @exception IllegalArgumentException if <code>currencyCode</code> is not
+     * @return the {@code Currency} instance for the given currency code
+     * @throws    NullPointerException if {@code currencyCode} is null
+     * @throws    IllegalArgumentException if {@code currencyCode} is not
      * a supported ISO 4217 code.
      */
     public static Currency getInstance(String currencyCode) {
@@ -349,7 +351,7 @@ public final class Currency implements Serializable {
     }
 
     /**
-     * Returns the <code>Currency</code> instance for the country of the
+     * Returns the {@code Currency} instance for the country of the
      * given locale. The language and variant components of the locale
      * are ignored. The result may vary over time, as countries change their
      * currencies. For example, for the original member countries of the
@@ -364,16 +366,16 @@ public final class Currency implements Serializable {
      * specified, the currency from the "cu" extension supersedes the implicit one
      * from the "rg" extension.
      * <p>
-     * The method returns <code>null</code> for territories that don't
+     * The method returns {@code null} for territories that don't
      * have a currency, such as Antarctica.
      *
-     * @param locale the locale for whose country a <code>Currency</code>
+     * @param locale the locale for whose country a {@code Currency}
      * instance is needed
-     * @return the <code>Currency</code> instance for the country of the given
+     * @return the {@code Currency} instance for the country of the given
      * locale, or {@code null}
-     * @exception NullPointerException if <code>locale</code>
+     * @throws    NullPointerException if {@code locale}
      * is {@code null}
-     * @exception IllegalArgumentException if the country of the given {@code locale}
+     * @throws    IllegalArgumentException if the country of the given {@code locale}
      * is not a supported ISO 3166 country code.
      */
     public static Currency getInstance(Locale locale) {
@@ -536,7 +538,7 @@ public final class Currency implements Serializable {
      * @param locale the locale for which a display name for this currency is
      * needed
      * @return the symbol of this currency for the specified locale
-     * @exception NullPointerException if <code>locale</code> is null
+     * @throws    NullPointerException if {@code locale} is null
      */
     public String getSymbol(Locale locale) {
         LocaleServiceProviderPool pool =
@@ -563,7 +565,7 @@ public final class Currency implements Serializable {
      * -1 is returned.
      *
      * @return the default number of fraction digits used with this currency
-    */
+     */
     public int getDefaultFractionDigits() {
         return defaultFractionDigits;
     }
@@ -631,7 +633,7 @@ public final class Currency implements Serializable {
      * @param locale the locale for which a display name for this currency is
      * needed
      * @return the display name of this currency for the specified locale
-     * @exception NullPointerException if <code>locale</code> is null
+     * @throws    NullPointerException if {@code locale} is null
      * @since 1.7
      */
     public String getDisplayName(Locale locale) {
@@ -661,6 +663,7 @@ public final class Currency implements Serializable {
     /**
      * Resolves instances being deserialized to a single instance per currency.
      */
+    @java.io.Serial
     private Object readResolve() {
         return getInstance(currencyCode);
     }
@@ -937,16 +940,16 @@ public final class Currency implements Serializable {
      * - oldCurrencyNumericCode: numeric code for old currencies
      * - newCurrencyNumericCode: numeric code for new currencies, 0 for countries
      *   that are not changing currencies
-    */
+     */
     private static class SpecialCaseEntry {
 
-        final private long cutOverTime;
-        final private String oldCurrency;
-        final private String newCurrency;
-        final private int oldCurrencyFraction;
-        final private int newCurrencyFraction;
-        final private int oldCurrencyNumericCode;
-        final private int newCurrencyNumericCode;
+        private final long cutOverTime;
+        private final String oldCurrency;
+        private final String newCurrency;
+        private final int oldCurrencyFraction;
+        private final int newCurrencyFraction;
+        private final int oldCurrencyNumericCode;
+        private final int newCurrencyNumericCode;
 
         private SpecialCaseEntry(long cutOverTime, String oldCurrency, String newCurrency,
                 int oldCurrencyFraction, int newCurrencyFraction,
@@ -1039,9 +1042,9 @@ public final class Currency implements Serializable {
      */
     private static class OtherCurrencyEntry {
 
-        final private String currencyCode;
-        final private int fraction;
-        final private int numericCode;
+        private final String currencyCode;
+        private final int fraction;
+        private final int numericCode;
 
         private OtherCurrencyEntry(String currencyCode, int fraction,
                 int numericCode) {
@@ -1076,11 +1079,11 @@ public final class Currency implements Serializable {
      * - date: cutover date
      */
     private static class CurrencyProperty {
-        final private String country;
-        final private String currencyCode;
-        final private int fraction;
-        final private int numericCode;
-        final private String date;
+        private final String country;
+        private final String currencyCode;
+        private final int fraction;
+        private final int numericCode;
+        private final String date;
 
         private CurrencyProperty(String country, String currencyCode,
                 int fraction, int numericCode, String date) {
@@ -1195,5 +1198,3 @@ public final class Currency implements Serializable {
     }
 
 }
-
-
