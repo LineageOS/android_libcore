5724e517b9dcfc6540a67783a069af6c8cd29f16
Pengtao He <hepengtao@xiaomi.com>
2023-07-04 17:51:03 +0800

    Add SOL_PACKET and PACKET_IGNORE_OUTGOING  constants in OsConstants.
    The AF_PACKET socket bind to ETH_P_ALL will
    invalidate the zero-copy send feature. Add
    PACKET_IGNORE_OUTGOING option for
    Os.setsockoptInt to avoid copying send packets.
    
    Bug: 289845107
    Test: build
    Change-Id: I070dcb0ec9ec5c125cd9cd2763a8302eefc38798
    Signed-off-by: Pengtao He <hepengtao@xiaomi.com>

--- luni/src/main/java/android/system/OsConstants.java
+++ luni/src/main/java/android/system/OsConstants.java
@@ -686,6 +686,7 @@ public final class OsConstants {
     public static final int SOCK_STREAM = placeholder();
     public static final int SOL_SOCKET = placeholder();
     public static final int SOL_UDP = placeholder();
+    public static final int SOL_PACKET = placeholder();
     public static final int SO_BINDTODEVICE = placeholder();
     public static final int SO_BROADCAST = placeholder();
     public static final int SO_DEBUG = placeholder();
@@ -710,6 +711,7 @@ public final class OsConstants {
     public static final int SO_SNDLOWAT = placeholder();
     public static final int SO_SNDTIMEO = placeholder();
     public static final int SO_TYPE = placeholder();
+    public static final int PACKET_IGNORE_OUTGOING = placeholder();
     /**
      * Bitmask for flags argument of
      * {@link splice(java.io.FileDescriptor, Int64Ref, FileDescriptor, Int64Ref, long, int)}.
